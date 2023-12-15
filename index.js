43173a6816dbd57dfbcbe0450e1c673e9d02e53b
TimothyMcCann <timothysmccann@gmail.com>
2023-12-15 10:15:36 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, query){
+    return drivers.filter(driver =>
+        driver.toLowerCase() === query.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, query){
+    return drivers.filter(driver =>
+        driver.slice(0, 1) === query.slice(0, 1)
+    )
+}
+
+function matchName(drivers, query){
+    return drivers.filter(driver =>
+        driver.name === query)
+}
